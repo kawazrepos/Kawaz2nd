390498749c6c4a28b2b5be76558469194ba2abef
Λlisue <lambdalisue@hashnote.net>
2011-09-19 02:13:32 +0900

    Version up to 0.31415rc1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ License:
     limitations under the License.
 """
 __AUTHOR__ = "lambdalisue (lambdalisue@hashnote.net)"
-__VERSION__ = "0.314159rc1"
+__VERSION__ = "0.31415rc1"
 from setuptools import setup, find_packages
 
 def read(filename):
