32b92bdaf75c390d342e9386c314aaaf5639c899
giginet <giginet.net@gmail.com>
2011-12-21 03:29:48 +0900

    Ignore spam comment history.

--- src/Kawaz/mcomments/history.py
+++ src/Kawaz/mcomments/history.py
@@ -10,5 +10,12 @@
 #
 from libwaz.contrib.history import site
 from libwaz.contrib.history.backends import CommentHistoryBackend
+from libwaz.contrib.history import backends
 from models import MarkItUpComment
+
+class EntryHistoryBackend(backends.BasicHistoryBackend):
+    def autodiscover(self, instance, *args, **kwargs):
+        if not instance.is_public:
+            return None
+        return super(EntryHistoryBackend, self).autodiscover(instance, *args, **kwargs)
 site.register(MarkItUpComment, CommentHistoryBackend)
